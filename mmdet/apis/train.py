ef284b62175ba7bf7db6dd87938c41d7b6e4caae
Masato Tsuchiya <malk271828@gmail.com>
2020-12-25 17:47:19 +0900

    update

--- mmdet/apis/train.py
+++ mmdet/apis/train.py
@@ -116,7 +116,7 @@ def train_detector(model,
             models = [CustomDataParallel(
                 model.cuda(cfg.gpu_ids[0]), device_ids=cfg.gpu_ids) for model in models]
             model = models[0]
-        elif cfg.coteaching_method=="per-object":
+        elif cfg.optimizer_config.coteaching_method=="per-object":
             models = [CustomDataParallel(
                 model.cuda(cfg.gpu_ids[0]), device_ids=cfg.gpu_ids) for model in models]
             model = models[0]
