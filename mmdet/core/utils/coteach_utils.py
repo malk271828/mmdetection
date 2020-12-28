8afcd36c96db4525b8e3798e7dc355d6fece77c7
Masato Tsuchiya <malk271828@gmail.com>
2020-12-28 21:10:57 +0900

    imp. distillation (not finished)

--- mmdet/core/utils/coteach_utils.py
+++ mmdet/core/utils/coteach_utils.py
@@ -4,9 +4,6 @@ from rich.progress import track
 from rich import pretty, print
 pretty.install()
 
-import torch
-import torch.nn.functional as F
-
 from mmdet.core import get_classes
 from mmcv.runner import load_checkpoint
 from mmcv.runner.hooks import HOOKS, OptimizerHook
@@ -18,8 +15,6 @@ from mmdet.utils import get_root_logger
 # import sys
 # sys.path.append("mmdet/models")
 # from builder import build_detector
-
-EPSILON = 1.0e-10
 
 @HOOKS.register_module()
 class CoteachingOptimizerHook(OptimizerHook):
@@ -52,7 +47,7 @@ class CoteachingOptimizerHook(OptimizerHook):
         if not (hasattr(runner, "models") and isinstance(runner.models, list)):
             runner.logger.warning("runner.models attribute must be list type in CoteachOptimizerHook. But got {0}".format(type(runner.models)))
 
-        for optimizer, loss in zip(runner.optimizers, losses_update):
+        for optimizer, loss in zip(runner.optimizers, runner.losses_update):
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -84,7 +79,6 @@ class DistillationOptimizerHook(OptimizerHook):
         self.beta = distill_config.beta
         self.gamma = distill_config.gamma
         self.temperature = distill_config.temperature
-        self.num_classes = 80
         self.use_focal = True
         self.use_adaptive = False
 
@@ -94,89 +88,13 @@ class DistillationOptimizerHook(OptimizerHook):
         if len(params) > 0:
             return clip_grad.clip_grad_norm_(params, **self.grad_clip)
 
-    def __before_train_iter(self, runner):
-        if self.teacher_model is None:
-            # init detector
-            self.teacher_model = build_detector(self.distill_config.model, test_cfg=self.distill_config.test_cfg)
-
-            # same logic to init_detector API
-            if checkpoint is not None:
-                    map_loc = "cpu"
-                    checkpoint = load_checkpoint(self.teacher_model, self.distill_config.checkpoint, map_location=map_loc)
-                    if 'CLASSES' in checkpoint['meta']:
-                        self.teacher_model.CLASSES = checkpoint['meta']['CLASSES']
-                    else:
-                        warnings.simplefilter('once')
-                        warnings.warn('Class names are not saved in the checkpoint\'s '
-                                    'meta data, use COCO classes by default.')
-                        self.teacher_model.CLASSES = get_classes('coco')
-            self.teacher_model.cfg = self.distill_config  # save the config in the model for convenience
-            self.teacher_model.to(runner.device)
-            self.teacher_model.eval()
-
-            if self.verbose > 0:
-                print(Fore.CYAN + "teacher model is loaded : {0}".format(checkpoint) + Style.RESET_ALL)
-
-            # register models to runner
-            runner.models.append(self.teacher_model)
-
     def after_train_iter(self, runner):
         if not (hasattr(runner, "models") and isinstance(runner.models, list)):
             runner.logger.warning("runner.models attribute must be list type in DistillationOptimizerHook. But got {0}".format(type(runner.models)))
 
-        # distinguish student/teacher loss and optimizer
-        student_optimizer, _ = runner.optimizers
-        (student_cls_logits, student_bbox_logits), (teacher_cls_logits, teacher_bbox_logits) = runner.logits
-        student_losses, _ = runner.outputs
-        student_cls_logits = torch.cat([logit.flatten() for logit in student_cls_logits])
-
-        # Calculate distillation loss
-        soft_log_probs = F.log_softmax(student_cls_logits.reshape(-1, self.num_classes) / self.temperature, dim=1)
-        soft_targets = F.softmax(teacher_cls_logits.reshape(-1, self.num_classes) / self.temperature, dim=1)
-        soft_kl_div = F.kl_div(soft_log_probs, soft_targets.detach(), reduction="none")
-
-        if self.use_focal:
-            # compute focal term
-
-            # https://kornia.readthedocs.io/en/latest/_modules/kornia/losses/focal.html#FocalLoss
-            if self.use_adaptive:
-                # Normal entropy is calculated by multiplying probability and log-probability
-                # https://discuss.pytorch.org/t/calculating-the-entropy-loss/14510
-                if self.verbose > 0:
-                    print("use automated adaptative distillation")
-                soft_log_targets = F.log_softmax(teacher_cls_logits.reshape(-1, self.num_classes) / self.temperature, dim=1)
-                entropy_target = torch.mean(- soft_targets * soft_log_targets, dim=1).unsqueeze(1).expand(-1, self.num_classes)
-                soft_distance = soft_kl_div + self.beta * entropy_target
-            else:
-                if abs(self.alpha) < EPSILON or abs(self.alpha - 1.0) < EPSILON:
-                    if self.verbose > 0:
-                        print("Alpha-balancing is disabled")
-                    soft_distance = - soft_log_probs * soft_targets.detach()
-                else:
-                    if self.verbose > 0:
-                        print("Alpha-balancing is enabled")
-                    soft_distance = - (np.log(self.alpha) + soft_log_probs) * (1 - self.alpha) * soft_targets.detach()
-            focal_term = torch.pow(1. - torch.exp( - soft_distance), self.gamma)
-
-            if self.normalized:
-                norm_factor = 1.0 / (focal_term.sum() + 1e-5)
-            else:
-                norm_factor = 1.0
-            if self.verbose > 0:
-                print("focal_term shape:{0} range[{1}, {2}]".format(focal_term.shape, torch.min(focal_term), torch.max(focal_term)))
-                print("norm_factor: {0}".format(norm_factor))
-            focal_distillation_loss = focal_term.reshape(-1, self.num_classes) * norm_factor * soft_kl_div
-
-            sum_focal_distillation_loss = focal_distillation_loss.sum() / num_pos
-            sum_classification_loss = classification_loss.sum()
-            sum_regression_loss = regression_loss.sum()
-            overall_loss = self.loss_wts.distill * sum_focal_distillation_loss + self.loss_wts.student * (sum_regression_loss + sum_classification_loss)
-        else:
-            overall_loss = 0.3 * student_losses.sum() + 0.7 * soft_kl_div
-
-        student_optimizer.zero_grad()
-        overall_loss.backward()
-        student_optimizer.step()
+        runner.optimizer.zero_grad()
+        runner.overall_loss.backward()
+        runner.optimizer.step()
 
         if self.grad_clip is not None:
             grad_norm = self.clip_grads(runner.model.parameters())
--- mmdet/runner/cooperative_train_runner.py
+++ mmdet/runner/cooperative_train_runner.py
@@ -9,6 +9,7 @@ from rich import pretty, print
 pretty.install()
 
 import torch
+import torch.nn.functional as F
 from torchviz import make_dot
 
 import mmcv
@@ -33,6 +34,9 @@ class CooperativeTrainRunner(EpochBasedRunner):
     """Cooperative Train Runner.
     This runner train models cooperatively.
     """
+
+    EPSILON = 1.0e-10
+
     def __init__(self,
                  models: list(),
                  batch_processor=None,
@@ -86,10 +90,12 @@ class CooperativeTrainRunner(EpochBasedRunner):
                     # define from default settings
                     self.rate_schedule = np.linspace(0, 0.8, self.max_epochs)
 
+            outputs = [model.train_step(data_batch, optimizer, **kwargs) for model, optimizer in zip(self.models, self.optimizers)]
+
             if isinstance(self.opt_hook, CoteachingOptimizerHook):
-                outputs = [model.train_step(data_batch, optimizer, **kwargs) for model, optimizer in zip(self.models, self.optimizers)]
                 if self.opt_hook.coteaching_method == "naive" or self.opt_hook.coteaching_method == "per-object":
-                    # [Han2018] and [Chadwick2019]
+                    # naive Co-teaching [Han2018]
+                    # per-object co-teaching [Chadwick2019]
                     zip_loss0 = zip(outputs[0]["losses"]["loss_cls"], outputs[0]["losses"]["loss_bbox"])
                     zip_loss1 = zip(outputs[1]["losses"]["loss_cls"], outputs[1]["losses"]["loss_bbox"])
 
@@ -115,9 +121,13 @@ class CooperativeTrainRunner(EpochBasedRunner):
 
                         # exchange data sample index
                         loss0_update = loss0[ind1_update]
-                        loss1_update = loss1[ind0_update]                        
+                        loss1_update = loss1[ind0_update]
+
+                        # pack
+                        self.losses_update = [torch.sum(loss0_update)/num_remember, torch.sum(loss1_update)/num_remember]
                     else:
-                        num_remember = int(remember_rate * len(loss0_sorted[1]))
+                        num_remember = int(remember_rate * len(loss0_sorted[argsort_dim]))
+                        divisor = len(loss0_sorted[0])
                         ind0_update=ind0_sorted[:, :num_remember]
                         ind1_update=ind1_sorted[:, :num_remember]
 
@@ -125,8 +135,8 @@ class CooperativeTrainRunner(EpochBasedRunner):
                         loss0_update = loss0[:, ind1_update]
                         loss1_update = loss1[:, ind0_update]                        
 
-                    # pack
-                    self.losses_update = [torch.sum(loss0_update)/num_remember, torch.sum(loss1_update)/num_remember]
+                        # pack
+                        self.losses_update = [torch.sum(loss0_update)/divisor, torch.sum(loss1_update)/divisor]
 
                 # model visualization
                 if verbose > 1:
@@ -145,10 +155,61 @@ class CooperativeTrainRunner(EpochBasedRunner):
                 self.log_buffer.update({"drop_rate": drop_rate})
 
             elif isinstance(self.opt_hook, DistillationOptimizerHook):
-                outputs = [model.train_step(data_batch, optimizer, **kwargs) for model, optimizer in zip(self.models, self.optimizers)]
-                self.logits = [model.forward_dummy(data_batch) for model, optimizer in zip(self.models, self.optimizers)]
+                # logits value is required because log probability should be computed with a temperature parameter.
+                logits = [model.forward_dummy(data_batch) for model, optimizer in zip(self.models, self.optimizers)]
+
+                # distinguish student/teacher loss and optimizer
+                student_optimizer = self.optimizers[0]
+                (student_cls_logits, student_bbox_logits), (teacher_cls_logits, teacher_bbox_logits) = logits
+                student_losses = outputs[0]
+                student_cls_logits = torch.cat([logit.flatten() for logit in student_cls_logits])
+
+                # Calculate distillation loss
+                soft_log_probs = F.log_softmax(student_cls_logits.reshape(-1, self.opt_hook.num_classes) / self.opt_hook.temperature, dim=1)
+                soft_targets = F.softmax(teacher_cls_logits.reshape(-1, self.opt_hook.num_classes) / self.opt_hook.temperature, dim=1)
+                soft_kl_div = F.kl_div(soft_log_probs, soft_targets.detach(), reduction="none")
+
+                if self.opt_hook.use_focal:
+                    # compute focal term
+
+                    # https://kornia.readthedocs.io/en/latest/_modules/kornia/losses/focal.html#FocalLoss
+                    if self.opt_hook.use_adaptive:
+                        # Normal entropy is calculated by multiplying probability and log-probability
+                        # https://discuss.pytorch.org/t/calculating-the-entropy-loss/14510
+                        if self.verbose > 0:
+                            print("use automated adaptative distillation")
+                        soft_log_targets = F.log_softmax(teacher_cls_logits.reshape(-1, self.opt_hook.num_classes) / self.opt_hook.temperature, dim=1)
+                        entropy_target = torch.mean(- soft_targets * soft_log_targets, dim=1).unsqueeze(1).expand(-1, self.opt_hook.num_classes)
+                        soft_distance = soft_kl_div + self.opt_hook.beta * entropy_target
+                    else:
+                        if abs(self.opt_hook.alpha) < self.EPSILON or abs(self.opt_hook.alpha - 1.0) < self.EPSILON:
+                            if self.verbose > 0:
+                                print("Alpha-balancing is disabled")
+                            soft_distance = - soft_log_probs * soft_targets.detach()
+                        else:
+                            if self.verbose > 0:
+                                print("Alpha-balancing is enabled")
+                            soft_distance = - (np.log(self.opt_hook.alpha) + soft_log_probs) * (1 - self.opt_hook.alpha) * soft_targets.detach()
+                    focal_term = torch.pow(1. - torch.exp( - soft_distance), self.opt_hook.gamma)
+
+                    if self.opt_hook.normalized:
+                        norm_factor = 1.0 / (focal_term.sum() + 1e-5)
+                    else:
+                        norm_factor = 1.0
+                    if self.verbose > 0:
+                        print("focal_term shape:{0} range[{1}, {2}]".format(focal_term.shape, torch.min(focal_term), torch.max(focal_term)))
+                        print("norm_factor: {0}".format(norm_factor))
+                    focal_distillation_loss = focal_term.reshape(-1, self.opt_hook.num_classes) * norm_factor * soft_kl_div
+
+                    sum_focal_distillation_loss = focal_distillation_loss.sum() / num_pos
+                    sum_classification_loss = classification_loss.sum()
+                    sum_regression_loss = regression_loss.sum()
+                    self.overall_loss = self.loss_wts.distill * sum_focal_distillation_loss + self.loss_wts.student * (sum_regression_loss + sum_classification_loss)
+                else:
+                    self.overall_loss = 0.3 * student_losses.sum() + 0.7 * soft_kl_div
+
             else:
-                raise Exception("expected optimizer type is either CooperativeOptimizerHook or DistillationOptimizerHook. But got: {0}".format(type(opt_hook)))
+                raise Exception("expected optimizer type is either CooperativeOptimizerHook or DistillationOptimizerHook. But got: {0}".format(type(self.opt_hook)))
         else:
             outputs = [model.val_step(data_batch, optimizer, **kwargs) for model, optimizer in zip(self.models, self.optimizers)]
 
